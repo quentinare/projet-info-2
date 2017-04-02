35aa2fe9f596f63b845b5753b564617fce6af8b9
QuentinAré <quentin.are@live.fr>
2017-04-02 17:41:58 +0200

    humain plus plateau
    ajout de la classe par héritage de humain
    sp ans humain dans la classe

--- plateau.h
+++ plateau.h
@@ -8,13 +8,21 @@ class Plateau
 {
 private:
     std::vector<std::vector<Case> > m_plateau;
+    std::vector<Case> m_stockage_cas;
+    std::vector<int> m_scores;
 
 public:
     Plateau();
     ~Plateau();
-    void afficherTab();
+
     void setTab(std::vector<std::vector<Case> > plateau);
     std::vector<std::vector<Case> > getTab() const;
+
+    std::vector<Case> possibilite_placement (std::vector<std::vector<Case> > plateau);
+    void calcul_encadrement(std::vector<std::vector<Case> > plateau);
+    void afficherTab(std::vector<std::vector<Case> > plateau);
+    void savegare_partie();
+    void navigation_choix();
 
 };
 
