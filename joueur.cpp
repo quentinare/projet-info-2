5e9f1c3d932006c575f4f2250c35e6016ffdd59a
QuentinAré <quentin.are@live.fr>
2017-04-02 16:27:32 +0200

    joueur
    ajous de la classe joueur

---
+++ joueur.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "joueur.h"
+
+Joueur();
+~Joueur();
+
+void Joueur::setnom(std::string nom)
+{
+    m_nom = nom;
+}
+void Joueur::setscore (int score)
+{
+    m_score = score;
+}
+
+std::string Joueur::getnom() const
+{
+    return m_nom;
+}
+int Joueur::getscore () const
+{
+    return m_score;
+}
