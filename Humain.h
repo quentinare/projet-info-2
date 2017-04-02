df2b91bcc295ca2d1f5f60e160d4ae93d7698cd1
QuentinAré <quentin.are@live.fr>
2017-04-02 20:33:34 +0200

    réglage dun pb de commits

--- Humain.h
+++ Humain.h
@@ -1,11 +1,16 @@
 #ifndef HUMAIN_H_INCLUDED
 #define HUMAIN_H_INCLUDED
 #include "joueur.h"
+#include "case.h"
 
 class Humain : Joueur
 {
 public:
-    void choix_coup();
-    void affichage_coup();
+    Humain();
+    ~Humain();
+
+    void choix_coup(std::vector<std::vector<Case> > monplateau, std::vector<Case> stockagecas);
+    void affichage_coup(std::vector<std::vector<Case> > monplateau);
+
 };
 #endif // HUMAIN_H_INCLUDED
