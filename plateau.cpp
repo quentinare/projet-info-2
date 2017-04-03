8f63eddd63420b4b93c9bd2ba9ba8c05baef4a73
vgiot <valentin.giot@edu.ece.fr>
2017-04-04 00:07:41 +0200

    update plateau.cpp

--- plateau.cpp
+++ plateau.cpp
@@ -32,3 +32,38 @@ std::vector<std::vector<Case> > getTab() const
 {
     return m_plateau;
 }
+
+void Plateau :: deplacement()
+{
+    bool quit = false;
+    Console* pConsole = NULL;
+
+    // Alloue la mémoire du pointeur
+    pConsole = Console::getInstance();
+
+    // Affichage avec gotoligcol et couleur
+    pConsole->gotoLigCol(5, 10);
+    pConsole->setColor(COLOR_GREEN);
+    pConsole->setColor(COLOR_DEFAULT);
+
+    // Boucle événementielle
+    while (!quit)
+    {
+        // Si on a appuyé sur une touche du clavier
+        if (pConsole->isKeyboardPressed())
+        {
+            // Récupère le code ASCII de la touche
+            int key = pConsole->getInputKey();
+            std::cout << "touche = " << key << std::endl;
+
+            if (key == 27) // 27 = touche escape
+            {
+                quit = true;
+            }
+            if (key == e)
+        }
+    }
+
+    // Libère la mémoire du pointeur !
+    Console::deleteInstance();
+}
