f661547e088f4179d4e23fb4e34a4faa8c09e57e
DDiane <dianedumont96@yahoo.fr>
2017-04-11 17:43:51 +0200

    création de la fonction menu

--- main.cpp
+++ main.cpp
@@ -10,6 +10,37 @@
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    void menu()
+   {
+       int choixMenu;
+       
+       do 
+       {
+           std::cout << "Bienvenue dans le jeu d'Othello!" << std::endl;
+           std::cout << "Entrez le chiffres correspondants à votre choix:" << std::endl;
+           std::cout << "1.Jouer" << std::endl;
+           std::cout << "2.Règles" << std::endl;
+           std::cout << "3.Quitter" << std::endl;
+           
+           std::cin >> choixMenu;
+           
+           while (choixMenu != 1 && choixMenu != 2 && choixMenu != 3)
+           do
+           {
+               std::cout << "choix invalide, veuillez réessayer" << std::endl;
+               std::cin >> choixMenu;
+           }
+     
+           switch (choixMenu)
+           {
+               case 1:
+                   
+                   break;
+               case 2:
+                   break;
+               case 3:
+                   break;
+               
+   } 
     return 0;
 }
