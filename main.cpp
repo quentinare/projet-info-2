3d423f73217fad1ed2ac5a8abe16791b749f5fb4
vgiot <valentin.giot@edu.ece.fr>
2017-04-22 13:49:38 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
 #include "plateau.h"
 #include "case.h"
 #include "joueur.h"
@@ -20,12 +21,13 @@ int main()
 void monmenu()
 {
     int choixMenu = 0;
+    int i=0, j=0;
     Plateau monPlateau;
     std::vector<std::vector <Case> > tableau;
     tableau = monPlateau.initialise();
 
 
-    while (choixMenu != 3)
+    do
     {
         std::cout << "Bienvenue dans l'OTHELLO!" << std::endl;
         std::cout << "Veuillez choisir ce que vous souhaitez faire" << std::endl;
@@ -34,19 +36,75 @@ void monmenu()
         std::cout << "3. Quitter" << std::endl;
         std::cin >> choixMenu;
 
-        if (choixMenu == 1)
-                monPlateau.afficherTab(tableau);
+    }
+    while (choixMenu == 0);
 
-        if (choixMenu == 2)
-        {
-            std::cout << "Le but du jeu est d'encadrer les pions de l'autre joueur afin de les convertir" << std::endl;
-            std::cout << "Utiliser les touches s d f e pour deplacer le curseur" << std::endl;
-            std::cout << "Placer un pion sur une case en appuyant sur espace" << std::endl;
-        }
-
+    if (choixMenu == 1)
+    {
+        system("cls");
+        do {
+        monPlateau.afficherTab(tableau);
+        monPlateau.deplacement(i, j);
+       }
+       while (choixMenu == 1);
     }
 
+    if (choixMenu == 2)
+    {
+        std::cout << "\t OTHELLO GAME !!" << std::endl;
 
+        std::cout << "Objectif" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Le but du jeu est d'encadrer les pions de l'autre " << std::endl;
+        std::cout << "joueur afin de les convertir" << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "Regles du jeu" << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "1) Utiliser les touches s d f e pour deplacer le curseur" << std::endl;
+        std::cout << std::endl;
+        std::cout << "2) Placer un pion sur une case en appuyant sur espace" << std::endl;
+        std::cout << std::endl;
+        std::cout << "3) Vous ne pouvez placer un pion que a proximite" << std::endl;
+        std::cout << std::endl;
+        std::cout << "   d'un pion de votre adversaire" << std::endl;
+        std::cout << std::endl;
+        std::cout << "4) Votre score est le comptage du nombre de vos pions" << std::endl;
+        std::cout << std::endl;
+        std::cout << "   sur le plateau" << std::endl;
+        std::cout << std::endl;
+        std::cout << "5) La partie se termine lorsqu'un joueur ne peut plus placer" << std::endl;
+        std::cout << "   de pion" << std::endl;
+        std::cout << std::endl;
+        std::cout << "6) Le joueur ayant le plus grand score remporte la partie" << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "TAPER 1 POUR RETOURNER AU MENU" << std::endl;
+
+        std::cin >> choixMenu;
+
+        if (choixMenu == 1)
+        {
+            system("cls");
+            choixMenu = 0;
+
+            do
+            {
+                std::cout << "Bienvenue dans l'OTHELLO!" << std::endl;
+                std::cout << "Veuillez choisir ce que vous souhaitez faire" << std::endl;
+                std::cout << "1. JOUER " << std::endl;
+                std::cout << "2. Lire les regles du jeux" << std::endl;
+                std::cout << "3. Quitter" << std::endl;
+                std::cin >> choixMenu;
+
+            }
+            while (choixMenu == 0);
+
+        }
+    }
 
 }
 
